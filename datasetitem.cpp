546667e07d099b72e432b23542397062af66ddf3
Aaron Wang <aaronw94@gmail.con>
2018-12-05 18:39:29 -0600

    fixed?

--- datasetitem.cpp
+++ datasetitem.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>
 #include <sstream>
 
+
+/* Default Constructor */
+DataSetItem::DataSetItem()
+{
+	_label = 0;
+	_dim_maps = std::vector<std::map<int, int> > ;
+}
+
 /* Construct from values */
 
 DataSetItem::DataSetItem(int label, std::vector<std::map<int, int>> dim_maps)
@@ -17,3 +25,9 @@ DataSetItem::DataSetItem(int label, std::vector<std::map<int, int>> dim_maps)
 
 /* Move Constructor */ //Is this needed?
 
+/* Destructor */
+
+DataSetItem::~DataSetItem()
+{
+	_dim_maps.clear();
+}
