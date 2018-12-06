89a9ef0362f4a1ca78e2ea6bfa879d96845e0a2d
Aaron Wang <aaronw94@gmail.con>
2018-12-06 17:26:08 -0600

    string tokenizer

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -3,10 +3,12 @@
 #include "datasetitem.h"
 #include "file_parser.h"
 
-
 int main(int argc, char ** argv){
 	std::vector<DataSetItem> v;
 	v = file_parser(argv[0]);
-	std::cout << v.get_label << v.get_dim_maps;		
+	for (auto it = v.begin(); it!= v.end(); ++it)
+	{
+		std::cout << it->get_label << it->get_dim_maps;	
+	}
 	return 0;
 }
