44ac96143306f07a60c5749688fc3727885fb88d
Aaron Wang <ajwang5@illinois.edu>
2018-12-09 15:44:35 -0600

    fixing decision node decs

--- decision_node.h
+++ decision_node.h
@@ -9,7 +9,7 @@
 class DecisionNode
 {
 	public:
-		DecisionNode(Question new_question,bool leaf = false);
+		DecisionNode(Question new_question,bool leaf);
 		bool eval_question(std::pair<int,int> example);
 		DecisionNode * true_branch;
 		DecisionNode * false_branch;
@@ -17,6 +17,6 @@ class DecisionNode
 		Question _question;
 		bool _leaf;
 		std::map<int, int> counts; //tracks which counts of labels at leaf node -> choose majority, random for tiebreak
-}
+};
 
-#endif DECISION_NODE_H_
+#endif 
